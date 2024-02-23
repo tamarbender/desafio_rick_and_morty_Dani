3bf539d3018374c68abbdcd835b7ff57895d2367
Yohanna8 <yohanna.cavalcanti@dock.tech>
2024-02-23 20:15:22 -0300

    Adicionando rota episode/id

--- app/app.py
+++ app/app.py
@@ -1,28 +1,46 @@
 from flask import render_template, Flask, request
-from app.controller.controllers import get_episodes
+from app.controller.controllers import get_episodes, get_episode_by_id
+import requests
+import concurrent.futures
 
 app = Flask(__name__)
-
 
 @app.route('/episodes')
 def episodes():
     page = request.args.get('page', 1, type=int)
     episodes, total_pages = get_episodes(page)
-
     return render_template('episodes.html', episodes=episodes, current_page=page, total_pages=total_pages)
-
-
 
 @app.route('/episode/<int:id>')
 def episode(id):
-    return
+    episode = get_episode_by_id(id)
+    
+    character_urls = episode['characters']
+    character_data = get_character_data(character_urls)
 
+    return render_template('episode.html', episode=episode, character_data=character_data)
 
-@app.route('/locations')
-def locations():
-    return
+def get_character_data(character_urls):
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        future_to_url = {executor.submit(get_character_data_single, url): url for url in character_urls}
+        character_data = []
+        for future in concurrent.futures.as_completed(future_to_url):
+            character_url = future_to_url[future]
+            try:
+                character_info = future.result()
+                character_data.append(character_info)
+            except Exception as exc:
+                print(f'A requisição para {character_url} falhou: {exc}')
+                character_data.append(None)
+    return character_data
 
-
-@app.route('/location/<int:id>')
-def location(id):
-    return
+def get_character_data_single(character_url):
+    response = requests.get(character_url)
+    if response.status_code == 200:
+        character_data = response.json()
+        return {
+            'name': character_data['name'],
+            'image': character_data['image']
+        }
+    else:
+        return None
--- app/controller/controllers.py
+++ app/controller/controllers.py
@@ -1,6 +1,5 @@
 import requests
 
-
 def get_episodes(page=1, page_size=20):
     url = f"https://rickandmortyapi.com/api/episode?page={page}&page_size={page_size}"
     response = requests.get(url)
@@ -9,8 +8,16 @@ def get_episodes(page=1, page_size=20):
     return episodes, data["info"]["pages"]
 
 
-def get_episode_by_id():
-    pass
+def get_episode_by_id(id, page=1, page_size=10):
+    url = f"https://rickandmortyapi.com/api/episode/{id}"
+    response = requests.get(url)
+    episode_details = response.json()
+    data = episode_details['episode'].split('E')
+
+    episode_details['season'] = data[0][1:]
+    episode_details['episode'] = data[1]
+
+    return episode_details
 
 
 def get_locations():
